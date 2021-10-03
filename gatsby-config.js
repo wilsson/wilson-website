3e94f5dcaf88cb123bddb24bd100e43fab7ed46e
wilsson <wilsonft83@gmail.com>
2021-10-02 23:55:54 -0500

    feat: add typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,12 @@ module.exports = {
         path: `${__dirname}/src/posts/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
     'gatsby-plugin-mdx',
   ],
 }

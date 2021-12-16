e52306e7b3575d6498f2828480afbc50cfab07ae
wilsson <wilsonft83@gmail.com>
2021-12-16 15:33:23 -0500

    chore: add slash for route and active menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import { Link } from 'gatsby'
 export const Header = () => {
   return(
     <header className="container m-auto flex mt-4 mb-16">
-      <Link to="/" className="text-white text-sm bg-black px-2.5 h-8 flex items-center rounded cursor-pointer mr-4">Home</Link>
-      <Link to="/blog" className="text-white text-sm h-8 px-2.5 flex items-center rounded cursor-pointer hover:bg-black mr-4">Blog</Link>
+      <Link to="/" activeClassName="bg-black" className="text-white text-sm px-2.5 h-8 flex items-center rounded cursor-pointer hover:bg-black mr-4">Home</Link>
+      <Link to="/blog/" activeClassName="bg-black" className="text-white text-sm px-2.5 h-8 flex items-center rounded cursor-pointer hover:bg-black mr-4">Blog</Link>
     </header>
   )
 }

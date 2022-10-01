016ad3c5423d8531d6a5f25e92a916f7d4a8b8aa
wilsson <wilsonft83@gmail.com>
2022-10-01 00:54:43 -0500

    feat: add sanity

---
+++ lib/sanity.ts
@@ -0,0 +1,4 @@
+import createImageUrlBuilder from '@sanity/image-url';
+import { sanityConfig } from './sanity-config';
+
+export const urlFor = (source) => createImageUrlBuilder(sanityConfig).image(source);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,39 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
+import Image from 'next/future/image';
+import { urlFor } from '../lib/sanity';
+import { sanityClient } from '../lib/sanity-server';
+
+function HomePage(props) {
+  console.log('props', props);
+  return(
+    <div>
+      <h1>Welcome to Next.js + Sanity.io!</h1>
+      <hr />
+      <ul>
+        {props.posts.map(({ title, coverImage }) => {
+          return(
+            <li key={title}>
+              <h2>{title}</h2>
+              <Image
+                src={urlFor(coverImage).url()}
+                width="128"
+                height="72"
+                alt="github"
+              />
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default HomePage
+
+export async function getStaticProps() {
+  const posts = await sanityClient.fetch(`*[_type == "post"]`);
+  return {
+    props: {
+      posts
+    } 
+  };
+}

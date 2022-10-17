dcf383befb07019ed291db41d19e7ca3aef77f87
wilsson <wilsonft83@gmail.com>
2022-10-16 19:23:15 -0500

    feat: add next-seo

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,16 +1,17 @@
+import { useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { parseISO, format } from 'date-fns';
-import readingTime from 'reading-time';
-import PostInstance from '~/lib/post';
 import { MDXRemote } from 'next-mdx-remote';
+import readingTime from 'reading-time';
+import { NextSeo } from 'next-seo';
+import { EllipsisHorizontalIcon } from '@heroicons/react/24/solid';
+import PostInstance from '~/lib/post';
 import { LikeIcon } from '~/components/LikeIcon';
 import { Navbar } from '~/components/Navbar';
 import { Footer } from '~/components/Footer';
 import { urlFor } from '~/lib/sanity'
 import { useLikes } from '~/lib/useLikes';
 import { useViews } from '~/lib/useViews';
-import { useEffect } from 'react';
-import { EllipsisHorizontalIcon } from '@heroicons/react/24/solid';
 
 const optionsAnimateLike = {
   hide: {
@@ -47,43 +48,58 @@ const Post = ({ post, mdxSource }) => {
   }, []);
 
   return(
-    <main className="px-8">
-      <Navbar />      
-      <article className="mx-auto max-w-2xl w-full">
-        <h1 className="text-white text-4xl font-[800] mb-2 mt-10">{post.title}</h1>
-        <div className="mb-4 flex justify-between flex-col sm:flex-row">
-          <p className="text-gray-400 text-sm mb-1">
-            {format(parseISO(post.date), 'MMMM dd, yyyy')} • {readingTime(post.content).text} 
-          </p>
-           <div className="flex space-x-2 items-center">
-            <p className="text-gray-400 text-sm">
-              {(viewsIsLoading || viewsIsError) ? <EllipsisHorizontalIcon  className="w-[18px] animate-pulse inline-block"/> : dataViews.views} views
-              {" • "}
-              {(likesIsLoading || likesIsError) ? <EllipsisHorizontalIcon  className="w-[18px] animate-pulse inline-block"/> : dataLikes.likes} likes
+    <>
+      <NextSeo 
+        title={post.title}
+        canonical={`https://wilsonft83.dev/blog/${post.slug}`}
+        openGraph={{
+          title: post.title,
+          images: [
+            {
+              url: urlFor(post.coverImage).url(),
+              alt: post.title,
+            }
+          ],
+        }}
+      />
+      <main className="px-8">
+        <Navbar />      
+        <article className="mx-auto max-w-2xl w-full">
+          <h1 className="text-white text-4xl font-[800] mb-2 mt-10">{post.title}</h1>
+          <div className="mb-4 flex justify-between flex-col sm:flex-row">
+            <p className="text-gray-400 text-sm mb-1">
+              {format(parseISO(post.date), 'MMMM dd, yyyy')} • {readingTime(post.content).text} 
             </p>
-            <div className={`cursor-pointer transition-all bg-white/10 p-1 rounded-lg flex ${dataLikes?.userLike ? 'bg-gradient-to-tl from-purple-800 to-indigo-400': ''}`}>
-                <button onClick={handleLike} className="relative focus:outline-none">
-                  <motion.div
-                    className="absolute w-full"
-                    animate={dataLikes?.userLike ? 'show' : 'hide'}
-                    variants={optionsAnimateLike}
-                    initial={false}
-                  >
-                    <p className="text-white">👍</p>
-                  </motion.div>
-                  <LikeIcon className={`w-[18px] h-[18px] ${dataLikes?.userLike ? 'text-white': 'text-gray-400'}`} />
-                </button>
+            <div className="flex space-x-2 items-center">
+              <p className="text-gray-400 text-sm">
+                {(viewsIsLoading || viewsIsError) ? <EllipsisHorizontalIcon  className="w-[18px] animate-pulse inline-block"/> : dataViews.views} views
+                {" • "}
+                {(likesIsLoading || likesIsError) ? <EllipsisHorizontalIcon  className="w-[18px] animate-pulse inline-block"/> : dataLikes.likes} likes
+              </p>
+              <div className={`cursor-pointer transition-all bg-white/10 p-1 rounded-lg flex ${dataLikes?.userLike ? 'bg-gradient-to-tl from-purple-800 to-indigo-400': ''}`}>
+                  <button onClick={handleLike} className="relative focus:outline-none">
+                    <motion.div
+                      className="absolute w-full"
+                      animate={dataLikes?.userLike ? 'show' : 'hide'}
+                      variants={optionsAnimateLike}
+                      initial={false}
+                    >
+                      <p className="text-white">👍</p>
+                    </motion.div>
+                    <LikeIcon className={`w-[18px] h-[18px] ${dataLikes?.userLike ? 'text-white': 'text-gray-400'}`} />
+                  </button>
+              </div>
+            
             </div>
-           
           </div>
-        </div>
-        <div className="prose dark:prose-invert max-w-none">
-          <img src={urlFor(post.coverImage).url()} alt="" />
-          <MDXRemote {...mdxSource} />
-        </div>
-      </article>
-      <Footer />
-    </main>
+          <div className="prose dark:prose-invert max-w-none">
+            <img src={urlFor(post.coverImage).url()} alt={post.title} />
+            <MDXRemote {...mdxSource} />
+          </div>
+        </article>
+        <Footer />
+      </main>
+    </>
   )
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,49 +3,53 @@ import { Footer } from '~/components/Footer';
 import PostInstace from '~/lib/post';
 import Link from 'next/link';
 import { PostPreview } from '~/components/PostPreview';
+import { NextSeo } from 'next-seo';
 
 function HomePage({ posts }) {
   return(
-    <div>
-      <main className="px-8">
-        <div className="mx-auto max-w-2xl flex justify-between items-start mt-28 flex-col flex-col-reverse sm:flex-row">
-          <div className="sm:pr-14">
-            <h1 className="text-white text-4xl font-[800] mb-2">Wilson Flores</h1>
-            <h2 className="text-gray-300 mb-4">Sotfware Engineer FullStack at <a href="https://www.pedidosya.com" className="text-red-200 underline font-medium">PedidosYa</a></h2>
-            <p className="text-gray-400 mb-8">In this place I will share everything I have learned in my technology career.</p>
-            <div className="flex space-x-7">
-              <Link href="/about">
-                <div className="group flex space-x-2 cursor-pointer">
-                  <div className="shadow-lg group-hover:scale-125 group-hover:shadow-purple-900 transition-all bg-gradient-to-tl from-purple-800 to-indigo-400  p-1 rounded-lg">
-                    <IdentificationIcon className="w-[18px] text-white" />
+    <>
+      <NextSeo description="In this place I will share everything I have learned in my technology career." />
+      <div>
+        <main className="px-8">
+          <div className="mx-auto max-w-2xl flex justify-between items-start mt-28 flex-col flex-col-reverse sm:flex-row">
+            <div className="sm:pr-14">
+              <h1 className="text-white text-4xl font-[800] mb-2">Wilson Flores</h1>
+              <h2 className="text-gray-300 mb-4">Sotfware Engineer FullStack at <a href="https://www.pedidosya.com" className="text-red-200 underline font-medium">PedidosYa</a></h2>
+              <p className="text-gray-400 mb-8">In this place I will share everything I have learned in my technology career.</p>
+              <div className="flex space-x-7">
+                <Link href="/about">
+                  <div className="group flex space-x-2 cursor-pointer">
+                    <div className="shadow-lg group-hover:scale-125 group-hover:shadow-purple-900 transition-all bg-gradient-to-tl from-purple-800 to-indigo-400  p-1 rounded-lg">
+                      <IdentificationIcon className="w-[18px] text-white" />
+                    </div>
+                    <div className="text-gray-300">About</div>
                   </div>
-                  <div className="text-gray-300">About</div>
-                </div>
-              </Link>
-              <Link href="/blog">
-                <div className="group flex space-x-2 cursor-pointer">
-                  <div className="shadow-lg group-hover:scale-125 group-hover:shadow-purple-900 transition-all bg-gradient-to-tl from-purple-800 to-indigo-400 p-1 rounded-lg">
-                    <DocumentIcon className="w-[18px] text-white" />
+                </Link>
+                <Link href="/blog">
+                  <div className="group flex space-x-2 cursor-pointer">
+                    <div className="shadow-lg group-hover:scale-125 group-hover:shadow-purple-900 transition-all bg-gradient-to-tl from-purple-800 to-indigo-400 p-1 rounded-lg">
+                      <DocumentIcon className="w-[18px] text-white" />
+                    </div>
+                    <div className="text-gray-300">Blog</div>
                   </div>
-                  <div className="text-gray-300">Blog</div>
-                </div>
-              </Link>
+                </Link>
+              </div>
+            </div>
+            <img className="grayscale-[70%] w-28 rounded-full mb-6" src="/avatar.jpeg" />
+          </div>
+
+          <div className="mx-auto max-w-2xl mt-10">
+            <h3 className="text-2xl text-gray-300">All Posts</h3>
+            <div className="mt-4">
+              {posts.map((props, key) => (
+                <PostPreview {...props} key={key}/>
+              ))}
             </div>
           </div>
-          <img className="grayscale-[70%] w-28 rounded-full mb-6" src="/avatar.jpeg" />
-        </div>
-
-        <div className="mx-auto max-w-2xl mt-10">
-          <h3 className="text-2xl text-gray-300">All Posts</h3>
-          <div className="mt-4">
-            {posts.map((props, key) => (
-              <PostPreview {...props} key={key}/>
-            ))}
-          </div>
-        </div>
-        <Footer />
-      </main>
-    </div>
+          <Footer />
+        </main>
+      </div>
+    </>
   )
 }
 
